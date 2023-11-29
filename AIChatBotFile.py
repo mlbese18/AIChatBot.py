dc0cde7a20655f1eb09e4203d4cf2f491cac159d
mlbese18 <mlbese18@smumn.edu>
2023-11-29 10:52:23 -0600

    Update AIChatBotFile.py

---
+++ AIChatBotFile.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import filedialog
+import PyPDF2
+import os
+import re
+import nltk
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+#input the 3k word file
+
+global userInput
+userInput = ''
+#Ask for user input question
+
+while userInput != "EXIT":
+    print("placeholder")
+    #Placeholder
