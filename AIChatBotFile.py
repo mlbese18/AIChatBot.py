aa7819281319eece897b986949069279ee4cee0c
mlbese18 <mlbese18@smumn.edu>
2023-12-04 18:12:09 -0600

    Update AIChatBotFile.py

--- AIChatBotFile.py
+++ AIChatBotFile.py
@@ -4,6 +4,10 @@ import PyPDF2
 import os
 import re
 import nltk
+from tkinter import *
+
+import ttkbootstrap as tb # to install do: pip install ttkbootstrap
+from ttkbootstrap.scrolled import ScrolledText
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
